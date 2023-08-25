048040a1a5b352c87916184cd30b96939035f85d
Ubuntu <ubuntu@ip-172-31-45-80.ap-northeast-2.compute.internal>
2023-08-25 18:52:05 +0900

    static web hosting in nestjs server

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use('/',express.static(path.join(__dirname,'../frontend/build')))
   await app.listen(80);
 }
 bootstrap();
